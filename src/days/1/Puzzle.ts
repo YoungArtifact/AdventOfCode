dbbfde1ca26d44e2975bd6966911582f0cbb1002
Hydra <armin.hydra@gmail.com>
2023-12-02 00:24:09 +0100

    Complete day 1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,13 +1,66 @@
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
+  const regex = new RegExp('[0-9]')
+  let total : number = 0
+
+  for (let line of input.split('\n')) {
+    let numbers : string = ''
+    let two : string = ''
+    for (let digit of line) {
+      if (regex.test(digit)) {
+        numbers += digit
+        if (numbers.length > 2) {
+          let first : string = numbers[0]
+          let last : string = numbers[numbers.length - 1]
+
+          numbers = first + last
+        }
+      }
+  }
+  if (numbers.length == 1) numbers += numbers
+  
+  total += Number(numbers)
+  }
+
+  return total;
 };
 
 const expectedFirstSolution = 'solution 1';
 
 const second = (input: string) => {
-  console.log(input);
-  return 'solution 2';
+  const regex = new RegExp('[0-9]')
+  let total : number = 0
+
+  for (let line of input.split('\n')) {
+    let numbers : string = ''
+    let two : string = ''
+
+    line = line.replace(/one/gi, 'one1one')
+      .replace(/two/gi, 'two2two')
+      .replace(/three/gi, 'three3three')
+      .replace(/four/gi, 'four4four')
+      .replace(/five/gi, 'five5five')
+      .replace(/six/gi, 'six6six')
+      .replace(/seven/gi, 'seven7seven')
+      .replace(/eight/gi, 'eight8eight')
+      .replace(/nine/gi, 'nine9nine')
+
+    for (let digit of line) {
+      if (regex.test(digit)) {
+        numbers += digit
+        if (numbers.length > 2) {
+          let first : string = numbers[0]
+          let last : string = numbers[numbers.length - 1]
+
+          numbers = first + last
+        }
+      }
+  }
+  if (numbers.length == 1) numbers += numbers
+
+  total += Number(numbers)
+  }
+
+  return total;
 };
 
 const expectedSecondSolution = 'solution 2';
